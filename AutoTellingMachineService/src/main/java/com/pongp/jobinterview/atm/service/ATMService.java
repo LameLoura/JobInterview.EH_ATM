9417f45cee8ab889f33966680155bed8d7df33c9
Khun Pong <Lame Loura>
2018-06-10 14:20:37 +0700

    TDD : develope featurue on failing test cases

--- AutoTellingMachineService/src/main/java/com/pongp/jobinterview/atm/service/ATMService.java
+++ AutoTellingMachineService/src/main/java/com/pongp/jobinterview/atm/service/ATMService.java
@@ -1,11 +1,8 @@
 package com.pongp.jobinterview.atm.service;
 
 import java.util.List;
-import java.math.*;
+import java.util.stream.Collectors;
 
-import org.apache.el.lang.ELArithmetic.BigDecimalDelegate;
-
-import java.util.ArrayList;
 import com.pongp.jobinterview.atm.model.BankNote;
 import com.pongp.jobinterview.atm.model.BankNoteBatch;
 import com.pongp.jobinterview.atm.service.repository.IRepository;
@@ -34,12 +31,33 @@ public class ATMService
 	 * @return {@link BankNoteBatch} represent how many of each note withdrawer get
 	 * @throws InvalidWithdrawException
 	 */
-	public BankNoteBatch withdrawMoney( int amount )  throws InvalidWithdrawException
+	public BankNoteBatch withdrawMoney( int remainingAmount )  throws InvalidWithdrawException
 	{
-		validateWithdrawAmount( amount );
+		validateWithdrawAmount( remainingAmount );
 		
-		//TODO implement withdraw logic
-		return new BankNoteBatch();
+        	BankNoteBatch notesProvidedToWithdrawer = new BankNoteBatch();
+        
+        	// loop from the largest available note -> try to fill request with largest note possible first
+        	List<BankNote> descendingAvailableNotes = getAvailableBankNote().stream()
+        		.sorted((item1, item2) -> item2.getValue() - item1.getValue()).collect(Collectors.toList());
+        	for (BankNote note : descendingAvailableNotes)
+        	{
+        	    // keep it simple: keep subtracting from withdrawal amount as long as that note  is available and can fill-in the remaining amount
+        	    // TODO performance improvement on TDD refactor
+        	    while (remainingAmount > 0 && note.getNoteCount() > 0 && note.getValue() <= remainingAmount )
+        	    {
+        		remainingAmount -= note.getValue();
+        		note.descreaseNoteCount();
+        		notesProvidedToWithdrawer.addNote(note.getValue());
+        	    }
+        	}
+        	
+        	if( remainingAmount!= 0 )
+        	{
+        	    throw new InvalidWithdrawException( WithdrawErrorMessage.INSUFFCIENT_NOTE.getErrorMessage() );
+        	}
+        	
+		return notesProvidedToWithdrawer;
 	}
 	
 	private void validateWithdrawAmount( int amount ) throws InvalidWithdrawException
